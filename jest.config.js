1cf898f9a45760f8a4ab86b9f57315d799bb7232
Gabriel Souza <contato@devgabrielsouza.com.br>
2025-05-17 13:11:07 -0300

    chore: add Jest and Nodemon configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'apps',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
